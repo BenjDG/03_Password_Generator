1f2e524ab97c22306328643a97c46dbf01756e2f
Ben Galloway <bdgalloway85@gmail.com>
2020-09-25 22:14:57 -0500

    build user validation check loop

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -16,33 +16,51 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   console.log('started');
-//variable for character set
+  //variable for character set
 
-var characterSet = "";
-var lowercaseLetters = false;
-var uppercaseLetters = false;
-var numericCharacters = false;
-var specialCharacters = false;
+  var characterSet = "";
+  var lowercaseLetters = false;
+  var uppercaseLetters = false;
+  var numericCharacters = false;
+  var specialCharacters = false;
+  var inputCheck = false;
 
-//prompt choose length of password 8-128
-var passwordLength = prompt("");
+  //prompt choose length of password 8-128
+  var passwordLength = prompt("For password length enter a number between 8 - 128.");
+  //console.log(passwordLength);
 
-//check did they enter a number between 8-128
+  var numberOfChar = Number(passwordLength);
+  //console.log(passwordLength);
 
+  console.log(Number.isInteger(numberOfChar));
+  // check for positive integer between 8-128
+  while (inputCheck === false) {
+    if (Number.isInteger(numberOfChar) && passwordLength >= 8 && passwordLength <= 128) {
 
+      console.log('Yay');
+      inputCheck === true;
+    } else {console.log('error')
+    var passwordLength = prompt("For password length enter a number between 8 - 128.");  
+    var numberOfChar = Number(passwordLength);
+  
+  }
 
-//prompt do you want lowercase?
-//prompt do you want uppercase?
-//prompt do you want numeric?
-//prompt do you want special characters?
+    console.log("test1");
+  }
 
-//check that at least one type of character was selected.
+console.log('moving on');
+  //prompt do you want lowercase?
+  //prompt do you want uppercase?
+  //prompt do you want numeric?
+  //prompt do you want special characters?
 
-//build character set
+  //check that at least one type of character was selected.
 
-//random generator for picking characters from variable iterate once for each number of characters
+  //build character set
 
-//return password
+  //random generator for picking characters from variable iterate once for each number of characters
+
+  //return password
 
 
 
