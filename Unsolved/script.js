61967c02c6dd33cbd629ef7da7ac05306948fe89
Ben Galloway <bdgalloway85@gmail.com>
2020-09-25 21:26:20 -0500

    add variables

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -12,3 +12,43 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword() {
+  console.log('started');
+//variable for character set
+
+var characterSet = "";
+var lowercaseLetters = false;
+var uppercaseLetters = false;
+var numericCharacters = false;
+var specialCharacters = false;
+
+//prompt choose length of password 8-128
+var passwordLength = prompt("");
+
+//check did they enter a number between 8-128
+
+
+
+//prompt do you want lowercase?
+//prompt do you want uppercase?
+//prompt do you want numeric?
+//prompt do you want special characters?
+
+//check that at least one type of character was selected.
+
+//build character set
+
+//random generator for picking characters from variable iterate once for each number of characters
+
+//return password
+
+
+
+
+
+
+
+
+}
